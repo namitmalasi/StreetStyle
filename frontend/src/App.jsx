a1da13606680349c489746e368031a2bb136485b
Namit Malasi <namitmalasi@gmail.com>
2024-12-05 15:13:57 +0530

    adding routes and navbar component in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,32 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Cart from "./pages/Cart";
+import Product from "./pages/Product";
+import PlaceOrder from "./pages/PlaceOrder";
+import Orders from "./pages/Orders";
+import Collection from "./pages/Collection";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
